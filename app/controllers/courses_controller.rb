2c3cbf4ac9dbf4c326906c96358b5b3f7de6030e
Matheus Koiti <msilva@caiena.net>
2016-05-18 17:54:31 -0300

    creating home link

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -20,7 +20,7 @@ class CoursesController < ApplicationController
 
     respond_to do |format|
       if @course.save
-        format.html { redirect_to @course, notice: 'Course was successfully created.' }
+        format.html { redirect_to @course, notice: 'Curso criado com sucesso.' }
         format.json { render :show, status: :created, location: @course }
       else
         format.html { render :new }
@@ -32,7 +32,7 @@ class CoursesController < ApplicationController
   def update
     respond_to do |format|
       if @course.update(course_params)
-        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
+        format.html { redirect_to @course, notice: 'Curso editado com sucesso.' }
         format.json { render :show, status: :ok, location: @course }
       else
         format.html { render :edit }
@@ -44,7 +44,7 @@ class CoursesController < ApplicationController
   def destroy
     @course.destroy
     respond_to do |format|
-      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
+      format.html { redirect_to courses_url, notice: 'Curso removido cm sucesso.' }
       format.json { head :no_content }
     end
   end
